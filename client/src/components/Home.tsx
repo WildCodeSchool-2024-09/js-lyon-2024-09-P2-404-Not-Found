f32fea653e5df84689ff104291b6a168690e983c
michaelgirardet <michaelgirardet@outlook.com>
2024-11-20 11:04:21 +0100

    home component with style, all the css remains to be unified

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import "../styles/Home.css";
+import "../styles/question.css";
+import "../styles/Ingredient.css";
+import Chef from "../images/logo-chef.png";
 
 function Home() {
   const [search, setSearch] = useState("");
@@ -17,18 +21,25 @@ function Home() {
 
   return (
     <section className="Questioncontainer">
-      <label htmlFor="research-select">Please choose a search type</label>
-      <select
-        onChange={(event) => setSearch(event.target.value)}
-        name="search-type"
-        id="search-select"
-      >
-        <option value="">Please choose search type</option>
-        <option value="Category">Category</option>
-        <option value="Country">Country</option>
-        <option value="Ingredient">Ingredient</option>
-        <option value="Random">Random</option>
-      </select>
+      <div className="inside-question">
+        <div className="back-chef">
+          <img src={Chef} alt="chef" className="image" />
+        </div>
+        <div>
+          <h1>What are we eating today?</h1>
+          <select
+            onChange={(event) => setSearch(event.target.value)}
+            name="search-type"
+            id="search-select"
+          >
+            <option value="">Please choose search type</option>
+            <option value="Category">Category</option>
+            <option value="Country">Country</option>
+            <option value="Ingredient">Ingredient</option>
+            <option value="Random">Random</option>
+          </select>
+        </div>
+      </div>
     </section>
   );
 }
